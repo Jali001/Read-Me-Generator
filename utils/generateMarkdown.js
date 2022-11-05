1e68bf5b1c5b457aacbe23b025824a282a1ac6b5
Junnun_Ali <junnun.b97@gmail.com>
2022-11-04 20:21:53 -0400

    adding code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-    
+function renderLicenseBadge(license) 
+{
+    if (data.license)
 }
 
 // TODO: Create a function that returns the license link
